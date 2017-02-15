328a489edf747416d9cf7fce8c11ccecc5819c25
Shahrukh <shahrukh@webklipper.com>
2017-02-15 13:24:56 +0530

    sample push and inapp callback implementation

--- src/android/WebEngagePlugin.java
+++ src/android/WebEngagePlugin.java
@@ -21,14 +21,36 @@ import android.location.Location;
 import android.os.Bundle;
 
 import com.webengage.sdk.android.WebEngage;
+import com.webengage.sdk.android.callbacks.PushNotificationCallbacks;
+import com.webengage.sdk.android.actions.render.PushNotificationData;
+import com.webengage.sdk.android.callbacks.InAppNotificationCallbacks;
 
-public class WebEngagePlugin extends CordovaPlugin {
+public class WebEngagePlugin extends CordovaPlugin implements PushNotificationCallbacks, InAppNotificationCallbacks{
     
     private static final String TAG = "WebEngagePlugin";
+    private static CordovaWebView webView;
+
+    private Map<String,Object> pushOptions = new HashMap<String,Object>();
+    private static final String PUSH_SMALL_ICON = "smallIcon";
+    private static final String PUSH_LARGE_ICON = "largeIcon";
+    private static final String PUSH_SOUND = "sound";
+    private static final String PUSH_VIBRATION = "vibration";
+    private static final String PUSH_ACCENT_COLOR = "accentColor";
+    private static final String PUSH_SHOULD_RENDER = "shouldRender";
+    private static final String PUSH_SHOULD_DELEGATE_CLICK = "shouldDelegateClick";
+
+
+    private Map<String,Object> inappOptions = new HashMap<String, Object> ();
+    private static final String INAPP_SHOULD_RENDER = "shouldRender";
+    private static final String INAPP_SHOULD_DELEGATE_CLICK = "shouldDelegateClick";
+
+
+    private Map<String,Object> globalOptions = new HashMap<String, Object>();
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
+        this.webView = webView;
         Log.v(TAG, "Intialized");
     }
     
@@ -37,9 +59,24 @@ public class WebEngagePlugin extends CordovaPlugin {
         Log.v(TAG,"Execute: "+action);
         
         if("engage".equals(action)) {
+            WebEngage.registerPushNotificationCallback(this);
+            WebEngage.registerInAppNotificationCallback(this);
+            WebEngage.registerLifeCycleCallback(this);
             WebEngage.engage(cordova.getActivity().getApplicationContext());
             WebEngage.get().analytics().start(cordova.getActivity());
             callbackContext.success();
+        }
+
+        if("pushOptions".equals(action)) {
+            pushOptions.put(args.getString(0), args.get(1));
+        }
+
+        if("inappOptions".equals(action)) {
+            inappOptions.put(args.getString(0), args.get(1));
+        }
+
+        if("globalOptions".equals(action)) {
+            globalOptions.put(args.getString(0), args.get(1));
         }
         
         return true;
@@ -56,6 +93,86 @@ public class WebEngagePlugin extends CordovaPlugin {
         Log.v(TAG,"Activity Stop");
         WebEngage.get().analytics().stop(cordova.getActivity());
         super.onStop();
+    }
+
+    @Override
+    public PushNotificationData onPushNotificationReceived(Context context, PushNotificationData notificationData) {
+
+        if(pushOptions.get(PUSH_SMALL_ICON)!=null) {
+            notificationData.setSmallIcon(pushOptions.get(PUSH_SMALL_ICON));
+        }
+        if(pushOptions.get(PUSH_LARGE_ICON) != null) {
+            notificationData.setLargerIcon(pushOptions.get(PUSH_LARGE_ICON));
+        }
+        if(pushOptions.get(PUSH_SOUND) != null && pushOptions.get(PUSH_SOUND) == true) {
+            notificationData.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+        }
+        if(pushOptions.get(PUSH_VIBRATION) != null && pushOptions.get(PUSH_VIBRATION) == true) {
+            notificationData.setVibrateFlag(true);
+        }
+        if(pushOptions.get(PUSH_ACCENT_COLOR) != null) {
+            notificationData.setAccentColor(pushOptions.get(PUSH_ACCENT_COLOR));
+        }
+        if(pushOptions.get(PUSH_SHOULD_RENDER) != null) {
+            notificationData.setShouldRender(pushOptions.get(PUSH_SHOULD_RENDER));
+        }
+
+        return notificationData;
+    }
+
+
+    @Override
+    public void onPushNotificationShown(Context context, PushNotificationData notificationData) {
+
+        webView.sendJavascript("javascript:webEngage.push.onCallbackReceived( 'shown', '" + notificationData.toString() + "');");
+    }
+
+     @Override
+    public boolean onPushNotificationClicked(Context context, PushNotificationData notificationData) {
+        
+        webView.sendJavascript("javascript:webEngage.push.onCallbackReceived( 'click', '" + notificationData.toString() + "','" + notificationData.getPrimeCallToAction().getId() + "');");
+        return pushOptions.get(PUSH_SHOULD_DELEGATE_CLICK) ? pushOptions.get(PUSH_SHOULD_DELEGATE_CLICK) : false;
+
+    }
+
+     @Override
+    public boolean onPushNotificationActionClicked(Context context, PushNotificationData notificationData, String buttonID) {
+        
+        webView.sendJavascript("javascript:webEngage.push.onCallbackReceived( 'click', '" + notificationData.toString() + "','" + buttonID + "');");
+        return pushOptions.get(PUSH_SHOULD_DELEGATE_CLICK) ? pushOptions.get(PUSH_SHOULD_DELEGATE_CLICK) : false;
+
+    }
+
+    @Override
+    public void onPushNotificationDismissed(Context context, PushNotificationData notificationData) {
+
+        webView.sendJavascript("javascript:webEngage.push.onCallbackReceived( 'dismiss', '" + notificationData.toString() + "');");
+    }
+
+    @Override
+    public InAppNotificationData onInAppNotificationPrepared(Context context, InAppNotificationData inAppNotificationData) {
+        
+        if(inappOptions.get(INAPP_SHOULD_RENDER) != null) {
+            inAppNotificationData.setShouldRender(inappOptions.get(INAPP_SHOULD_RENDER));
+        }
+        return inAppNotificationData;
+    }
+
+    @Override
+    public void onInAppNotificationShown(Context context, InAppNotificationData inAppNotificationData) {
+        webView.sendJavascript("javascript:webEngage.inapp.onCallbackReceived( 'shown', " + notificationData.getData() + ");");
+    }
+
+
+    @Override
+    public void onInAppNotificationDismissed(Context context, InAppNotificationData inAppNotificationData) {
+        webView.sendJavascript("javascript:webEngage.inapp.onCallbackReceived( 'dismiss', " + notificationData.getData() + ");");
+    }
+
+    @Override
+    public boolean onInAppNotificationClicked(Context context, InAppNotificationData inAppNotificationData, String actionId) {
+        webView.sendJavascript("javascript:webEngage.inapp.onCallbackReceived( 'click', " + notificationData.getData() + ",'" + actionId + "');");
+        return inappOptions.get(INAPP_SHOULD_DELEGATE_CLICK) ? inappOptions.get(INAPP_SHOULD_DELEGATE_CLICK) : false;;
     }
 
 }
--- www/WebEngagePlugin.js
+++ www/WebEngagePlugin.js
@@ -1,16 +1,20 @@
 var exec = require('cordova/exec');
 
 function WebEngagePlugin() {
-
+	this.push = new WebEngagePushChannel();
+	this.inapp = new WebEngageInAppChannel();
+	this.user = new WebEngageUserChannel();
+	this.analytics = new WebEngageAnalyticsChannel();
+	this._options = {};
 }
-
 
 WebEngagePlugin.prototype.engage = function() {
 	exec(null, null, "WebEngagePlugin", "engage",[]);
 };
 
-WebEngagePlugin.prototype.login = function(userId, success, error) {
-    exec(success, error, "WebEngagePlugin", "login", [userId]);
+WebEngagePlugin.prototype.options = function(key, value) {
+	this._options.key = value;
+	exec(null, null, "WebEngagePlugin", "globalOptions", [key, value]);
 };
 
 WebEngagePlugin.prototype.onActive = function(callback) {
@@ -23,7 +27,141 @@ WebEngagePlugin.prototype.onActive = function(callback) {
 		console.log("error");
 	}, "WebEngagePlugin", "onActive", [])
 	
+};
+
+
+function WebEngagePushChannel () {
+	this.callbacks = {};
+	this._options = {};
 }
+
+WebEngagePushChannel.prototype.options = function (key, value) {
+	this._options.key = value;
+	exec(null, null, "WebEngagePlugin", "pushOptions", [key, value]);
+};
+
+WebEngagePushChannel.prototype.onShown = function (callback) {
+	
+	if(!this.callbacks.hasOnProperty('shown')) {
+		this.callbacks.shown = [];
+	}
+
+	this.callbacks.shown.push(callback);
+};
+
+WebEngagePushChannel.prototype.onClick = function (callback) {
+	if(!this.callbacks.hasOnProperty('click')) {
+		this.callbacks.click = [];
+	}
+
+	this.callbacks.click.push(callback);
+}
+
+WebEngagePushChannel.prototype.onDismiss = function(callback) {
+	if(!this.callbacks.hasOnProperty('dismiss')) {
+		this.callbacks.dismiss = [];
+	}
+
+	this.callbacks.dismiss.push(callback);
+};
+
+WebEngagePushChannel.prototype.onCallbackReceived = function(type, data, id) {
+	if(type) {
+		switch(type) {
+			case 'shown' :
+				for(i = 0; i < this.callbacks.shown.length; i++) {
+					this.callbacks.shown[i](data);
+				}
+				break;
+
+			case 'click' :
+				for(i = 0; i < this.callbacks.click.length; i++) {
+					this.callbacks.click[i](data, id);
+				}
+				break;
+
+			case 'dismiss' :
+				for(i = 0; i < this.callbacks.dismiss.length; i++) {
+					this.callbacks.dismiss[i](data);
+				}
+				break;
+		}
+	}
+};
+
+function WebEngageInAppChannel () {
+	this.callbacks = {};
+	this._options = {};
+}
+
+WebEngageInAppChannel.prototype.options = function(key, value) {
+	this._options.key = value;
+	exec(null, null, "WebEngagePlugin", "inappOptions", [key, value]);
+};
+
+WebEngageInAppChannel.prototype.onShown = function (callback) {
+	
+	if(!this.callbacks.hasOnProperty('shown')) {
+		this.callbacks.shown = [];
+	}
+
+	this.callbacks.shown.push(callback);
+};
+
+WebEngageInAppChannel.prototype.onClick = function (callback) {
+	if(!this.callbacks.hasOnProperty('click')) {
+		this.callbacks.click = [];
+	}
+
+	this.callbacks.click.push(callback);
+}
+
+WebEngageInAppChannel.prototype.onDismiss = function(callback) {
+	if(!this.callbacks.hasOnProperty('dismiss')) {
+		this.callbacks.dismiss = [];
+	}
+
+	this.callbacks.dismiss.push(callback);
+};
+
+WebEngageInAppChannel.prototype.onCallbackReceived = function(type, data, id) {
+	if(type) {
+		switch(type) {
+			case 'shown' :
+				for(i = 0; i < this.callbacks.shown.length; i++) {
+					this.callbacks.shown[i](data);
+				}
+				break;
+
+			case 'click' :
+				for(i = 0; i < this.callbacks.click.length; i++) {
+					this.callbacks.click[i](data, id);
+				}
+				break;
+
+			case 'dismiss' :
+				for(i = 0; i < this.callbacks.dismiss.length; i++) {
+					this.callbacks.dismiss[i](data);
+				}
+				break;
+		}
+	}
+};
+
+function WebEngageUserChannel() {
+
+}
+
+WebEngageUserChannel.prototype.login = function(userId) {
+	exec(null, null, "WebEngagePlugin", "login", [userId]);
+};
+
+WebEngageUserChannel.prototype.logout = function() {
+	exec(null, null, "WebEngagePushChannel", "logout",[]);
+};
+
+
+
 if(typeof module != 'undefined' && module.exports) {
 	var WebEngagePlugin = new WebEngagePlugin();
 	module.exports = WebEngagePlugin;
